31a25277733914f7950e9a71ff7ed65e4b23293f
Tanaka Yuki <tnkyk0126@gmail.com>
2025-07-18 13:46:57 +0900

    LPデザイン修正

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,37 @@
 import Image from "next/image";
+import localImage from "../public/works/photo/IMGP2507-1.jpg";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen pb-20 gap-8 sm:p-10">
+    <div className="font-mono flex flex-col items-center gap-4 my-4 mx-10 md:mx-20">
       <header className="w-full">
-        <nav className="w-full flex justify-center">
+        <nav className="w-full flex justify-between text-gray-800 items-center">
           <div>
-            <div>tayutai - yuki</div>
-            <div>揺蕩</div>
+            <Link
+              href="/"
+              className="text-lg text-gray-500 hover:text-gray-700"
+            >
+              tayutai - yuki
+            </Link>
+          </div>
+          <div className="flex gap-4">
+            <Link href="/" className="text-gray-500 hover:text-gray-700">
+              photography
+            </Link>
+            <Link href="/" className="text-gray-500 hover:text-gray-700">
+              music
+            </Link>
+            <Link href="/" className="text-gray-500 hover:text-gray-700">
+              blog
+            </Link>
           </div>
         </nav>
       </header>
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          src="/works/photo/IMGP2507-1.jpg"
-          alt="Main Picture"
-          width={5934}
-          height={3945}
-        />
+      <main className="flex flex-col">
+        <Image src={localImage} alt="Main Picture" priority />
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+      <footer className="flex flex-wrap items-center justify-center text-gray-500 text-xs">
         @2025 tayutai All rights reserved.
       </footer>
     </div>
